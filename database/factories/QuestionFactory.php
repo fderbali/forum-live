cd02c0a746c5bf521e5588a3aaeef348f35b2530
Fahmi Derbali <fahmiderbali@gmail.com>
2021-06-19 01:02:09 -0400

    Factory and seed

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Category;
 use App\Models\Question;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class QuestionFactory extends Factory
@@ -21,8 +24,17 @@ class QuestionFactory extends Factory
      */
     public function definition()
     {
+        $sentence = $this->faker->sentence;
         return [
-            //
+            'title' => $sentence,
+            'slug' => Str::slug($sentence),
+            'body' => $this->faker->text,
+            'category_id' => function () {
+                return Category::all()->random();
+            },
+            'user_id' => function () {
+                return User::all()->random();
+            }
         ];
     }
 }
