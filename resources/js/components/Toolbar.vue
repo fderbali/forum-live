abd2e6aafa4e7ae11efc6eb784c97adc5e8303c6
Fahmi Derbali <fahmiderbali@gmail.com>
2021-07-11 22:45:37 -0400

    vuetify, vue router and initial components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    tile
+  >
+    <v-toolbar dense>
+      <v-toolbar-title>Live forum</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Forum</v-btn>
+      <v-btn flat>Ask Question</v-btn>
+      <v-btn flat>Category</v-btn>
+      <v-btn flat>Login</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-card>
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Single page forum</title>
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
@@ -12,50 +13,9 @@
 <body>
     <div id="app">
         <v-app app>
-            <v-container>
-                <v-btn color="primary">
-                    Primary
-                </v-btn>
-                <v-btn color="secondary">
-                    Secondary
-                </v-btn>
-                <v-btn color="error">
-                    Error
-                </v-btn>
-            </v-container>
-
-
-            <v-card
-                color="grey lighten-4"
-                flat
-                height="200px"
-                tile
-            >
-                <v-toolbar dense>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-                <v-toolbar-title>Title</v-toolbar-title>
-
-                <v-spacer></v-spacer>
-
-                <v-btn icon>
-                    <v-icon>mdi-magnify</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                    <v-icon>mdi-heart</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                    <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-                </v-toolbar>
-            </v-card>
-
-
+            <app-home></app-home>
         </v-app>
-
     </div>
-<script src="{{ mix('/js/app.js') }}"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
